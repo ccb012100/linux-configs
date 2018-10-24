56ff2ab7af514cd660a066ca48d67b5cb0a13e99
Christopher Bocardo <ccb012100@users.noreply.github.com>
2018-10-24 12:40:40 -0500

    adds aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,22 @@
+# zsh custom aliases
+# $ZSH_CUSTOM/aliases.zsh
+alias la='ls -A'
+
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias nano='nano -mc'
+
+# git aliases
+alias g='git'
+alias gc='git checkout'
+alias gb='git branch'
+alias gs='git status'
+
+setopt cdablevars
+ghub="/c/GitHub/"
+c="/c/"
